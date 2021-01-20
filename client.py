78721ca81b91fae973387575a719d3b76c8d6c3e
Adams Ijachi <adamsijachi9@gmail.com>
2021-01-20 03:43:11 +0100

    added gui

--- client.py
+++ client.py
@@ -2,46 +2,96 @@ import socket
 import pickle
 import threading
 import sys
+import tkinter as tk
 
 HEADERSIZE = 10
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.connect((socket.gethostname(), 1234))
+
+ip = "127.0.0.1"
+# ip = "9bae050eba34.ngrok.io"
+client_socket.connect((ip, 12345))
 username_exists = True
 
 nickname = ""
 
 def recive_msg():
-    while True:
-     
-        try:
-            message = client_socket.recv(1000)
+    while True:  
+        try: 
+            message = client_socket.recv(1024)
             msg_str = message.decode('utf-8').strip(' ')
-         
-            if msg_str.startswith('/nickname'):
-                msg_str = msg_str.split(" ") 
-                nickname = msg_str[1]
-                
+            if msg_str.startswith('/nickname') and "QUIT" not in msg_str:
+                try:
+                    nickname_display = msg_str[10:]
+                    msg_list.insert(tk.END, msg_str)
+                    top.title(f'Chat {nickname_display}')
+                except:
+                    break
+            elif "QUIT" in msg_str:
+                try:
+                    print(msg_str)
+                except:
+                    print('error')
+                    break
             else:
-                print(msg_str)
-        except:
-            print('Opps Server has been closed')
+                try:
+                    msg_list.insert(tk.END, msg_str)
+                    print(msg_str)
+                except:
+                    print('error')
+                    break
+
+        except :
+            server_msg = 'Opps Server something wrong with server try again later'
+            msg_list.insert(tk.END, server_msg)
             sys.exit()
+            break
  
 
-def write_msg():
-    while True:
-        msg = f'> : {input("")}'
-        if nickname:
-           msg = f'{nickname}: {input("")}'
-        msg_len = msg.split()
-        if len(msg_len) > 2:
-           client_socket.send(bytes(msg, "utf-8"))
+def write_msg(event=None):
+        
+        msg = my_msg.get()
+        
+        my_msg.set(" ")
+        if msg != " " and msg != "QUIT":
+            client_socket.send(bytes(msg, "utf-8"))
+        elif msg == "QUIT":
+            client_socket.send(bytes(msg, "utf-8"))
+            print('des')
+            sys.exit()
+            top.destroy()
         
 
+def on_closing(event=None):
+        top.destroy()
+        client_socket.send(bytes("QUIT", "utf-8"))
+        
 
+top = tk.Tk()
+top.title('Chat')
+
+message_frame = tk.Frame(top)
+my_msg = tk.StringVar()
+my_msg.set(" ")
+scrollbar = tk.Scrollbar(message_frame)
+
+msg_list = tk.Listbox(message_frame, height=15, width=50, yscrollcommand=scrollbar.set)
+
+scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
+msg_list.pack(side=tk.LEFT, fill=tk.BOTH)
+msg_list.pack()
+message_frame.pack()
+
+entry_field = tk.Entry(top, textvariable=my_msg)
+entry_field.bind("<Return>", write_msg )
+entry_field.pack()
+
+send_button = tk.Button(top, text='send', command=write_msg)
+send_button.pack()
+
+
+top.protocol("WM_DELETE_WINDOW", on_closing)
 
 recive_msg_thread = threading.Thread(target=recive_msg)
 recive_msg_thread.start()
-wirte_msg_thread = threading.Thread(target=write_msg)
-wirte_msg_thread.start()
+tk.mainloop()
 
--- server.py
+++ server.py
@@ -5,13 +5,13 @@ import threading
 
 HEADER = 10
 IP = "127.0.0.1"
-PORT = 1234
+PORT = 12345
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 server.bind((IP, PORT))
 
-server.listen()
+server.listen(5)
 
 client_list = []
 client_nicknames = []
@@ -23,8 +23,8 @@ def broadcast(msg):
     for client in client_list:
         client.send(msg)
 
-def private_msg(msg, reciver_nickname):
-    msg_turned_to_bytes = bytes(f'{reciver_nickname}  :  {msg}','utf-8')
+def private_msg(msg, reciver_nickname,senders_nickname):
+    msg_turned_to_bytes = bytes(f'[Private_msg]{senders_nickname}  :  {msg}','utf-8')
     index = client_nicknames.index(reciver_nickname)
     reciver_socket_address = client_list[index]
     reciver_socket_address.send(msg_turned_to_bytes)
@@ -36,24 +36,27 @@ def handle_msg(client):
     while True:
         try:
             message = client.recv(4096).decode('utf-8') ###  k : /private jack xup 
-            message_striped_without_nickname = message[ message.index(':') +1 : ].strip(' ') #### this removes the username and colon leaving  /private jack xup 
+            message_striped_without_nickname = message.strip(' ') #### this removes the username and colon leaving  /private jack xup 
             if message_striped_without_nickname.startswith('/'): 
                 msg_list = message_striped_without_nickname.split()
                 if msg_list[0] == '/private':
                     if msg_list[1] in client_nicknames:
+                        senders_index = client_list.index(client)
+                        senders_nickname = client_nicknames[senders_index]
                         reciver_nickname = msg_list[1]
                         senders_message = " ".join(msg_list[2:])
-                        private_msg(senders_message,reciver_nickname)
+                        private_msg(senders_message,reciver_nickname,senders_nickname )
                     else:
                         client.send(bytes('sorry seems like user does not exist', 'utf-8'))
 
             else:
                 index = client_list.index(client)
                 nickname = client_nicknames[index]
-                msg = bytes(f'{nickname}  {message}','utf-8')
+                msg = bytes(f'{nickname} : {message}','utf-8')
                 broadcast(msg)
     
         except:
+            
             index = client_list.index(client)
             client_list.remove(client)
             nickname = client_nicknames[index]
@@ -65,29 +68,34 @@ def handle_msg(client):
 def verify(client_socket):
     nickname_exist = False
     while not nickname_exist:
-            client_socket.send(bytes(f'Please enter a nickname : ', 'utf-8'))
-            nickname_list_form = client_socket.recv(4096).decode('utf-8').strip(' ').split(" ")
-            nickname = nickname_list_form[2]
-            if nickname not in client_nicknames:
-                client_list.append(client_socket)
-                client_nicknames.append(nickname)
-                client_socket.send(bytes(f'/nickname {nickname}', 'utf-8'))
-                notify = bytes(f'{nickname} has joined chat', 'utf-8')
-                broadcast(notify)
-                nickname_exist = True
-                thread = threading.Thread(target=handle_msg, args=(client_socket,))
-                thread.start()
-            else:
-                client_socket.send(bytes(f'Nick name taken ,Please enter a nickname : ', 'utf-8'))
-                
 
+        client_socket.send(bytes(f'Please enter a nickname : ', 'utf-8'))
+        nickname = client_socket.recv(1024).decode('utf-8').strip(' ')
+      
+
+        
+        if nickname not in client_nicknames:
+            
+            client_list.append(client_socket)
+            client_nicknames.append(nickname)
+            print(client_nicknames)
+            client_socket.send(bytes(f'/nickname {nickname}', 'utf-8'))
+            notify = bytes(f'{nickname} has joined chat', 'utf-8')
+            broadcast(notify)
+            nickname_exist = True
+            thread = threading.Thread(target=handle_msg, args=(client_socket,))
+            thread.start()
+        else:
+            client_socket.send(bytes(f'Nick name taken ,Please enter a nickname : ', 'utf-8'))
+        
 
 
 def recive_connection():
     while True:
-        client_socket , address = server.accept()
-        verify_thread = threading.Thread(target=verify, args=(client_socket,))
-        verify_thread.start()
-        
+            client_socket , address = server.accept()
+            verify_thread = threading.Thread(target=verify, args=(client_socket,))
+            verify_thread.start()
+
+
 thread = threading.Thread(target=recive_connection)
 thread.start()
